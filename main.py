eb2b4821cfb4bd0e2ef99519d8c5612b2cc3d6b4
Kyuwoong Lee <kwooler@gmail.com>
2023-05-24 16:33:11 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+evencnt = 0
+
+
+def main():
+    global evencnt
+    number = []
+    evencnt = 0
+
+    for i in range(10):
+        number.append(int(input()))
+    ##################################################
+    # Code your program here
+    ##################################################
+
+##
+
+
+if __name__ == '__main__':
+    main()
