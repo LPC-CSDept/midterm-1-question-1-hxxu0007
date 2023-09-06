0db15e491d4161d67df73a9de5d601b10ca2397e
Kyu Lee <kwooler@gmail.com>
2023-09-05 17:08:35 -0700

    M1Q1

--- main_test.py
+++ main_test.py
@@ -53,3 +53,49 @@ def test_main_2():
     # res = re.search(regex_string, main.evenlist)
     # assert res != None
     # print(res.group())
+
+
+def test_main_3():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '2\n4\n6\n5\n8\n10\n11\n12\n14\n20'
+    datastr = '1\n4\n6\n5\n8\n10\n11\n12\n14\n21'
+    sys.stdin = io.StringIO(datastr)
+
+    evencnt = main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+    print(f'Your output is {evencnt}')
+    assert evencnt == 3
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+
+
+def test_main_4():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '2\n4\n6\n4\n8\n10\n10\n12\n14\n20'
+    sys.stdin = io.StringIO(datastr)
+
+    evencnt = main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+    print(f'Your output is {evencnt}')
+    assert evencnt == 1
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
