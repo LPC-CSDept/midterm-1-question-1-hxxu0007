21aa6213999f99caf17710dd432f79b7a8336531
hxxu0007 <hxxu0007@zonemail.clpccd.edu>
2024-07-01 19:46:35 +0000

    ver3

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ def main():
 
     for i in range(10):
         number.append(int(input('Enter a number: ')))
-        if(not number[i].isalpha()):
-            print("The input value is not right.")
         if(number[i] % 2 == 0):
             evencnt += 1
 
